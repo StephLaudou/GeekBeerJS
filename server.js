4f2f437cb664bc2136c327b0c86c01a6127a0f05
StephLaudou <stephanie.laudou@gmail.com>
2019-05-21 23:24:45 +0200

    ajout module express nodemon

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
+var express = require('express');
+var app = express();
+
+/*app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Vous êtes à l\'accueil');
+});*/
+
+app.use("/css", express.static(__dirname +"/css"))
+app.use("/js", express.static(__dirname +"/client"))
+
+app.get("/", function (req,res){	
+	res.sendFile(__dirname + "/html/index.html");
+});
+
+app.listen(8080);
+
+
+
+
+
+
+/* SANS EXPRESS
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('Salut tout le monde !');
 });
-server.listen(8080);
+server.listen(8080);*/
