6cc9194a5e6bb366e15bacdf1fa11f58327b6f48
StephLaudou <stephanie.laudou@gmail.com>
2019-05-27 23:15:34 +0200

    integration des données du server BEGIN

--- client/client.js
+++ client/client.js
@@ -1,19 +1,87 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+getBeers();
+
+const getBeers = async function() {
+	try{
+		let response = await fetch ("/beers");
+		if (response.ok) {
+			let beers = response.json()
+			for (let beer of beers)
+				console.log(beer.name)
+				//createBeer(beer)
+			}
+		else {
+			alert('Server response' , response.status)
+		}	
+	} catch (e) {
+		alert(e)
+	}
+
+}
+
+
+const createBeer = function(beer) {
+			let list = document.querySelector(".liste")
+			//Creation de la balise Item
+			let item =  document.createElement("div");
+			item.classList.add("item");
+			list.appendChild(item);
+			//Creation de la balise Image
+			
+			//Creation de la balise label
+
+			//Creation du bouton ajouter
+
+
+
+
+}
+
 /*fetch("/beers")
-		.then(response => response.json() ) //--- on ne met pas au format JSON mais on recupere une reposne http qui com,ntient plein de chose et on en ectrait la partie json //--- equiv function(response){return response.json} JE METS LA RESPONSE SANS UNE VARIBLE notes
+		.then(response => response.json())
 		.then(beers => {
 			for (let beer of beers)
 				console.log(beer.name)
 		})*/
 
+/*DOC MOZILLA
+var myList = document.querySelector('ul');
+
+var myRequest = new Request('products.json');
+
+fetch(myRequest)
+  .then(function(response) { return response.json(); })
+  .then(function(data) {
+    for (var i = 0; i < data.products.length; i++) {
+      var listItem = document.createElement('li');
+      listItem.innerHTML = '<strong>' + data.products[i].Name + '</strong> can be found in ' +
+                           data.products[i].Location +
+                           '. Cost: <strong>£' + data.products[i].Price + '</strong>';
+      myList.appendChild(listItem);
+    }
+	});*/
+	
+/*GRAFIKART
+7:45
+try if resposne.ok else console response.status
+catch (e)
+
+*/
+
+/* OCR
+ajaxGet("http://localhost/javascript-web-srv/data/films.json", function (reponse) {
+    // Transforme la réponse en tableau d'objets JavaScript
+    var films = JSON.parse(reponse);
+    // Affiche le titre de chaque film
+    films.forEach(function (film) {
+        console.log(film.titre);
+    })
+});*/
+
 
 	let popupClose = document.querySelector(".popupClose");
 	popupClose.addEventListener('click',closePopup)
-	
-	/*let popupTriggers = document.querySelectorAll(".name");
-
-	popupTriggers.addEventListener('click',openPopup);*/
 
 document.querySelectorAll(".name").forEach(function(trigger){
 	trigger.addEventListener('click',openPopup)
